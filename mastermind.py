b621f066a7657e4556634a25f0369a37e290d948
Josh Thomas <joshuathomas2@mail.weber.edu>
2020-01-19 00:50:45 -0700

    computer_colors are always unique, added check for duplicate user_colors

--- mastermind.py
+++ mastermind.py
@@ -3,17 +3,20 @@ import random
 in_game = True
 valid_colors = ["R", "G", "B", "C", "M", "Y"]
 user_colors = []
-computer_colors = [random.choice(valid_colors), 
-                    random.choice(valid_colors), 
-                    random.choice(valid_colors), 
-                    random.choice(valid_colors)]
+computer_colors = ["", "", "", ""]
+
+available_colors = valid_colors.copy()
+for x in range(4):
+    random_number = random.randint(0, len(available_colors) - 1)
+    computer_colors[x] = available_colors[random_number]
+    available_colors.remove(available_colors[random_number])
 
 while in_game:
     valid_input = False
     clue = []
     computer_colors_copy = computer_colors.copy()
     while not valid_input:
-        user_colors = input(f"Please enter four colors for your guess. Colors: {''.join(valid_colors)} ").upper()
+        user_colors = input(f"Please enter four colors for your guess (do not use duplicate colors). Colors: {''.join(valid_colors)} ").upper()
         input_flag = False
 
         for color in user_colors:
@@ -24,7 +27,10 @@ while in_game:
         if len(user_colors) > 4:
             print("You entered too many colors. Please enter four colors.")
             input_flag = True
-        
+
+        if not (len(user_colors) == len(set(user_colors))):
+            input_flag = True
+            
         if input_flag:
             print(f"Your input {user_colors} was invalid. Please try again.")
         else:
